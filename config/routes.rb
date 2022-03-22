67e62801b08913dcb40fc70920a37fe602ed6c87
George Magdy <georgtriple@gmail.com>
2022-03-22 20:06:44 +0200

    Add routes for posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show] do
+    resources :posts, on: :show, only: [:index]
+  end
 end
