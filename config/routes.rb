58d138421410aa0fa45b1e37088ad86d8362ec25
George Magdy <georgtriple@gmail.com>
2022-03-22 20:14:14 +0200

    Add users index as default action

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
     resources :posts, on: :show, only: [:index, :show]
   end
+
+  get '/', to: redirect('/users')
 end
