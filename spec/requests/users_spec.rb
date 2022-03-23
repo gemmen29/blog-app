260c6b11ea6b94c83bb3fd4ce424efd6d9fbc898
George Magdy <georgtriple@gmail.com>
2022-03-23 21:59:27 +0200

    Add response status test for show users

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,18 +2,26 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
   describe "GET /index" do
-    before(:example) { get users_path } 
+    before(:example) { get '/users' } 
 
     it "Is response status correct" do
       expect(response).to have_http_status(:ok)
     end
 
     it "Is correct template rendered" do
-      expect(response).to render_template('index')
+      expect(response).to render_template(:index)
     end
 
     it "Is the body includes correct text" do
       expect(response.body).to include('Index')
+    end
+  end
+
+  describe "GET /show" do
+    before(:example) { get '/users/:user_id' } 
+
+    it "Is response status correct" do
+      expect(response).to have_http_status(:ok)
     end
   end
 end
