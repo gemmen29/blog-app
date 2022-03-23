6973467777b4a60d77469441503554f2523c42ca
George Magdy <georgtriple@gmail.com>
2022-03-23 20:22:32 +0200

    Add response status test for index users

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe "Users", type: :request do
+  describe "GET /index" do
+    before(:example) { get users_path } 
+
+    it "Is response status correct" do
+      expect(response).to have_http_status(:ok)
+    end
+  end
+end
