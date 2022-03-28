b4868eaefbe4b18aff5be373fbc5b44e5336630b
George Magdy <georgtriple@gmail.com>
2022-03-28 20:08:52 +0200

    Add five_most_recent_comments method

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,4 +6,8 @@ class Post < ApplicationRecord
   def update_post_user_counter
     user.update(user.posts_counter: user.posts.counter)
   end
+
+  def five_most_recent_comments
+    comments.last(5)
+  end
 end
