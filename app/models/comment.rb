a6b46a8b1b63a417c0e8f03941e725992deab611
George Magdy <georgtriple@gmail.com>
2022-03-28 20:17:18 +0200

    Add update_post_comment_counter method

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :user, foreign_key: :users_id
   belongs_to :post, foreign_key: :posts_id
+
+  def update_post_comment_counter
+    post.update(post.comments_counter: post.comments.counter)
+  end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -2,7 +2,7 @@ class Like < ApplicationRecord
   belongs_to :user, foreign_key: :users_id
   belongs_to :post, foreign_key: :posts_id
 
-  def update_post_user_counter
+  def update_post_like_counter
     post.update(post.likes_counter: post.likes.counter)
   end
 end
