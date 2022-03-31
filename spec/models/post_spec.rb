22d572b1d384e9e6019d55e83d1d3b1be572d0ac
George Magdy <georgtriple@gmail.com>
2022-03-31 23:14:17 +0200

    Add test subject valid for posts

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  subject { Post.create(title: 'Hello', text: 'This is my first post', likes_counter: 0, comments_counter: 0)}
+  subject { Post.new(user: User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.', posts_counter: 0), title: 'Hello', text: 'This is my first post', likes_counter: 0, comments_counter: 0)}
 
   before { subject.save }
 
@@ -18,5 +18,9 @@ RSpec.describe Post, type: :model do
   it 'comments_counter should be greater_than_or_equal zero' do 
     subject.comments_counter = -1
     expect(subject).to_not be_valid
+  end
+
+  it 'subject should be valid' do
+    expect(subject).to be_valid
   end
 end
