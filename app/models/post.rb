e76882345085670bffa67a194f7ac8ebbc01644b
George Magdy <georgtriple@gmail.com>
2022-03-31 22:15:22 +0200

    Add likes_counter validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,7 @@ class Post < ApplicationRecord
   after_save :update_post_user_counter
 
   validates :title, presence: true,  length: { maximum: 250 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def update_post_user_counter
     user.increment!(:posts_counter)
