a6c7b395d5aba3eabd2d6bce7bb67de71eda5a37
George Magdy <georgtriple@gmail.com>
2022-03-31 22:46:38 +0200

    Add tests for valid states for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  subject { User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')}
+  subject { User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.', posts_counter: 0)}
 
   before { subject.save }
 
@@ -10,8 +10,17 @@ RSpec.describe User, type: :model do
     expect(subject).to_not be_valid
   end
 
+  it 'name should be valid' do
+    expect(subject).to be_valid
+  end
+
   it 'posts_counter should be greater_than_or_equal zero' do 
     subject.posts_counter = -1
     expect(subject).to_not be_valid
+  end
+
+  it 'posts_counter be valid' do 
+    subject.posts_counter = 1
+    expect(subject).to be_valid
   end
 end
