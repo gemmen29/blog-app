65961a24a84730ab1db84de928b6d955bf4674e3
George Magdy <georgtriple@gmail.com>
2022-04-01 00:15:15 +0200

    Fix linters errors

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  subject { User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.', posts_counter: 0)}
+  subject { User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.', posts_counter: 0) }
 
   before { subject.save }
 
-  it 'name should be present' do 
+  it 'name should be present' do
     subject.name = nil
     expect(subject).to_not be_valid
   end
 
-  it 'posts_counter should be greater_than_or_equal zero' do 
+  it 'posts_counter should be greater_than_or_equal zero' do
     subject.posts_counter = -1
     expect(subject).to_not be_valid
   end
@@ -19,7 +19,7 @@ RSpec.describe User, type: :model do
     expect(subject).to be_valid
   end
 
-  it 'three_most_recent_posts should return a right result' do 
+  it 'three_most_recent_posts should return a right result' do
     subject.posts.create(title: 'Hello', text: 'This is my first post')
     subject.posts.create(title: 'Hello', text: 'This is my first post')
     subject.posts.create(title: 'Hello', text: 'This is my first post')
